56e0f65aaded5cbe8b1e565df71c9baa48024810
Black-Stormy <brookesy@internode.on.net>
2017-11-18 13:06:16 +1000

    updated port from 3000 to 8080 so we can get this thang on google cloud

--- runserver.js
+++ runserver.js
@@ -83,8 +83,8 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
   var os = require( 'os' );
 
 var networkInterfaces = os.networkInterfaces( );
